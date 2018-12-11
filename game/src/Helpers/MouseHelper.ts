6a4954bd7a6d9ea38acff4b05b4ff68bd26a0aa4
DjordyKoert <djordy.koert@live.nl>
2018-12-11 14:33:11 +0100

    MouseHelper getClick function

--- game/src/Helpers/MouseHelper.ts
+++ game/src/Helpers/MouseHelper.ts
@@ -1,5 +1,9 @@
 class MouseHelper {
 
+    private mDown: boolean
+    private mX: number
+    private mY: number
+
     constructor() {
         window.addEventListener("mousemove", this.mouseMove)
         window.addEventListener("mousedown", this.mouseDown)
@@ -10,12 +14,21 @@ class MouseHelper {
         return { mouseX: event.x, mouseY: event.y }
     }
 
-    public mouseDown = (event: MouseEvent) => {
-        return { mouseX: event.x, mouseY: event.y }
+    private mouseDown = (event: MouseEvent) => {
+        this.mDown = true
+        this.mX = event.x
+        this.mY = event.y
     }
 
-    public mouseUp = (event: MouseEvent) => {
-        return { mouseX: event.x, mouseY: event.y }
+    private mouseUp = (event: MouseEvent) => {
+        this.mDown = false
+        this.mX = event.x
+        this.mY = event.y
+    }
+
+    public getClick(): {click: boolean, x: number, y: number} {
+
+        return {click: this.mDown, x: this.mX, y: this.mY}
     }
 
 }
