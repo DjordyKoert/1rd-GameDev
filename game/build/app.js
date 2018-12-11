0e8f74000be9bcd1d0c70e741b33a4ec1d01cffa
DjordyKoert <djordy.koert@live.nl>
2018-12-11 21:36:43 +0100

    app.js

--- game/build/app.js
+++ game/build/app.js
@@ -12,10 +12,10 @@ class CanvasHelper {
         this._context.textBaseline = textBaseLine;
         this._context.fillText(text, xPos, yPos);
     }
-    writeImageToCanvas(Src, xPos, yPos) {
+    writeImageToCanvas(Src, xPos, yPos, imgWidth, imgHeight) {
         let image = new Image();
         image.addEventListener('load', () => {
-            this._context.drawImage(image, xPos, yPos);
+            this._context.drawImage(image, xPos, yPos, imgWidth, imgHeight);
         });
         image.src = Src;
     }
@@ -47,6 +47,13 @@ class CanvasHelper {
             }
         });
     }
+    moveTo(xPos, yPos) {
+        this._context.moveTo(xPos, yPos);
+    }
+    lineTo(xPos, yPos) {
+        this._context.lineTo(xPos, yPos);
+        this._context.stroke();
+    }
 }
 class App {
     constructor(canvasElem) {
@@ -68,9 +75,15 @@ class BaseView {
         this._canvasHelper = new CanvasHelper(canvas);
         this._homeView = new HomeView(this._canvasHelper);
         this._BuilderView = new BuilderView(this._canvasHelper);
+        this._GameView = new GameView(this._canvasHelper);
     }
     render() {
         this._homeView.renderScreen();
+    }
+}
+class MathHelper {
+    static randomNumber(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
     }
 }
 class MouseHelper {
@@ -99,6 +112,40 @@ class MouseHelper {
 class BuilderView {
     constructor(canvas) {
         this.CanvasHelper = canvas;
+    }
+}
+class GameView {
+    constructor(canvas) {
+        this._screen = "gameScreen";
+        this.CanvasHelper = canvas;
+        this.xCoord = this.yCoord = 0;
+        this.lines = 50;
+        if (this.CanvasHelper.getWidth() > this.CanvasHelper.getHeight()) {
+            this.sqSize = this.CanvasHelper.getWidth() / this.lines;
+        }
+        else {
+            this.sqSize = this.CanvasHelper.getHeight() / this.lines;
+        }
+        this.tileImages = [
+            "./assets/images/earth_textures/grass.png",
+            "./assets/images/houses/house.png",
+        ];
+        this.renderGrid();
+    }
+    renderScreen() {
+    }
+    renderGrid() {
+        for (let line = 0; line < this.lines; line++) {
+            this.CanvasHelper.moveTo(0, this.yCoord);
+            this.CanvasHelper.lineTo(this.CanvasHelper.getWidth(), this.yCoord);
+            this.CanvasHelper.moveTo(this.xCoord, 0);
+            this.CanvasHelper.lineTo(this.xCoord, this.CanvasHelper.getHeight());
+            for (let i = 0; i < this.lines; i++) {
+                this.CanvasHelper.writeImageToCanvas(this.tileImages[MathHelper.randomNumber(0, this.tileImages.length - 1)], this.xCoord, this.sqSize * i, this.sqSize, this.sqSize);
+            }
+            this.xCoord += this.sqSize;
+            this.yCoord += this.sqSize;
+        }
     }
 }
 class HomeView {
