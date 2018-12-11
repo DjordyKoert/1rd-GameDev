7be174aaac3bc203306e122eaeb9f9d441835724
ArondeRijke <aronderijke@gmail.com>
2018-12-11 12:01:39 +0100

    added text function to HomeView

--- game/src/views/homeView.ts
+++ game/src/views/homeView.ts
@@ -1,16 +1,20 @@
 class HomeView extends BaseView{
     protected _screen: string = "homeScreen";
     private _context: CanvasRenderingContext2D;
+    protected CanvasHelper: CanvasHelper
 
-    public constructor(screen: string){
+    public constructor(screen: string, ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
         super(canvas)
         this._screen = screen;
+        this._context = ctx;
+        this.CanvasHelper = new CanvasHelper(canvas)
     }
 
     public homeScreen(){
-        this.writeTextToCanvas("BACK", 140, horizontalCenter, 150);
-
-
+        this.CanvasHelper.writeTextToCanvas("BACK", 24, 20, 20);
+        
+        this._context.fillStyle="#ffeda0";
+        this._context.fillRect(0,0,150,100)
     }
 
 }
