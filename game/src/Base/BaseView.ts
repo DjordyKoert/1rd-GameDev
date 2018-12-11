15958af329f690b815a972f127d429665ae8e056
DjordyKoert <djordy.koert@live.nl>
2018-12-11 21:31:26 +0100

    added gameview var

--- game/src/Base/BaseView.ts
+++ game/src/Base/BaseView.ts
@@ -2,11 +2,13 @@ class BaseView {
     private _canvasHelper: CanvasHelper
     private _homeView: HomeView
     private _BuilderView: BuilderView
+    private _GameView: GameView
     public constructor(canvas: HTMLCanvasElement){
         this._canvasHelper = new CanvasHelper(canvas)
         
         this._homeView = new HomeView(this._canvasHelper)
         this._BuilderView = new BuilderView(this._canvasHelper)
+        this._GameView = new GameView(this._canvasHelper)
         //this._homeView.homeScreen()
     }
     public render() {
