3c377808c1c229f8032a797d8d8ca9a1a32096af
DjordyKoert <djordy.koert@live.nl>
2018-12-11 21:30:59 +0100

    GameView creates a grid with random images

--- game/src/views/GameView.ts
+++ game/src/views/GameView.ts
@@ -5,24 +5,38 @@ class GameView {
     private yCoord: number
     private lines: number
     private sqSize: number
+    private tileImages: Array<string>
 
     public constructor(canvas: CanvasHelper) {
         this.CanvasHelper = canvas
-        this.xCoord = 0
-        this.yCoord = 0
-        this.lines = 10
-        this.sqSize = this.CanvasHelper.getWidth() / this.lines
+        this.xCoord = this.yCoord = 0
+        this.lines = 50
+        //Check screen size to make grids fit
+        if (this.CanvasHelper.getWidth() > this.CanvasHelper.getHeight()) {
+            this.sqSize = this.CanvasHelper.getWidth() / this.lines
+        }
+        else {
+            this.sqSize = this.CanvasHelper.getHeight() / this.lines
+        }
+        this.tileImages = [
+            "./assets/images/earth_textures/grass.png",
+            "./assets/images/houses/house.png",
+        ]
         this.renderGrid()
     }
     public renderScreen() {
     }
     public renderGrid() {
         for (let line = 0; line < this.lines; line++) {
-            console.log(this.xCoord, this.yCoord, line)
             this.CanvasHelper.moveTo(0, this.yCoord)
             this.CanvasHelper.lineTo(this.CanvasHelper.getWidth(), this.yCoord)
             this.CanvasHelper.moveTo(this.xCoord, 0)
             this.CanvasHelper.lineTo(this.xCoord, this.CanvasHelper.getHeight())
+            //this.CanvasHelper.createRect(this.xCoord, 0, this.sqSize, this.sqSize)
+            for (let i = 0; i < this.lines; i++) {
+                this.CanvasHelper.writeImageToCanvas(this.tileImages[MathHelper.randomNumber(0, this.tileImages.length - 1)], this.xCoord, this.sqSize * i, this.sqSize, this.sqSize)
+            }
+
             this.xCoord += this.sqSize
             this.yCoord += this.sqSize
         }
