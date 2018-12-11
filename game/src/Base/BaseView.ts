7843b1e40ab8580a27e31238561a5d92dac84f10
DjordyKoert <djordy.koert@live.nl>
2018-12-11 12:35:33 +0100

    made baseview working for multiple views

--- game/src/Base/BaseView.ts
+++ game/src/Base/BaseView.ts
@@ -1,9 +1,20 @@
 class BaseView {
-    private _canvasHelper: CanvasHelper
+    protected _canvasHelper: CanvasHelper
+    private _canvas: HTMLCanvasElement
+    private _HomeView: HomeView
+    private _BuilderView: BuilderView
 
     public constructor(canvas: HTMLCanvasElement){
-
         this._canvasHelper = new CanvasHelper(canvas)
+        this._canvas = canvas
 
+        this._HomeView = new HomeView(canvas)
+        this._BuilderView = new BuilderView(canvas)
+
+    }
+
+    public render(){
+        console.log("baseview render")
+        this._HomeView.render()
     }
 }
