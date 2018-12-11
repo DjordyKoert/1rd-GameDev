4a647f24cd4bce94bcd55c2d4ea742db76f34432
DjordyKoert <djordy.koert@live.nl>
2018-12-11 12:35:50 +0100

    views are easier to make now

--- game/src/views/BuilderView.ts
+++ game/src/views/BuilderView.ts
@@ -1,6 +1,13 @@
-class BuilderView extends BaseView {
+class BuilderView extends BaseView{
+    protected _screen: string = "homeScreen";
+    //private _context: CanvasRenderingContext2D;
+    //protected CanvasHelper: CanvasHelper
 
-    protected constructor(canvas: HTMLCanvasElement){
+    public constructor(/*screen: string, ctx: CanvasRenderingContext2D,*/ canvas: HTMLCanvasElement){
         super(canvas)
+        // this._screen = screen;
+        // this._context = ctx;
+        // this.CanvasHelper = new CanvasHelper(canvas)
+        //this.homeScreen()
     }
 }
--- game/src/views/homeView.ts
+++ game/src/views/homeView.ts
@@ -1,21 +1,19 @@
 class HomeView extends BaseView{
     protected _screen: string = "homeScreen";
-    private _context: CanvasRenderingContext2D;
-    protected CanvasHelper: CanvasHelper
+    //private _context: CanvasRenderingContext2D;
+    //protected CanvasHelper: CanvasHelper
 
-    public constructor(screen: string, ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
+    public constructor(/*screen: string, ctx: CanvasRenderingContext2D,*/ canvas: HTMLCanvasElement){
         super(canvas)
-        this._screen = screen;
-        this._context = ctx;
-        this.CanvasHelper = new CanvasHelper(canvas)
-
+        // this._screen = screen;
+        // this._context = ctx;
+        // this.CanvasHelper = new CanvasHelper(canvas)
         this.homeScreen()
     }
 
     public homeScreen(){
-        this.CanvasHelper.writeTextToCanvas("BACK", 24, 20, 20);
-        
-        this.CanvasHelper.createRect(0,0,150,100)
+        this._canvasHelper.createRect(0,0,150,100)
+        this._canvasHelper.writeTextToCanvas("BACK", 24, 20, 20, "black");
     }
 
 }
