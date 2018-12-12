574f7a7ec7265ddd150053da92ccbbafadef28c3
ArondeRijke <aronderijke@gmail.com>
2018-12-12 09:14:30 +0100

    added buttons and planets to homeview

--- game/src/views/homeView.ts
+++ game/src/views/homeView.ts
@@ -7,7 +7,15 @@ class HomeView {
     }
 
     public renderScreen(){
-        this.CanvasHelper.createRect(0,0,150,100)
-        this.CanvasHelper.writeTextToCanvas("BArewwfCK", 24, 20, 20, "purple");
+        this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet.png", 250, 300)
+        this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet.png", 750, 400)
+        this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet.png", 1250, 200)
+        
+        this.CanvasHelper.writeButtonToCanvas(0,0,150,100, "BACK", 20);
+        this.CanvasHelper.writeButtonToCanvas(275,325,250,250, "", 0,"transparent");
+        this.CanvasHelper.writeButtonToCanvas(775,425,250,250, "", 0, "transparent");
+        this.CanvasHelper.writeButtonToCanvas(1275,225,250,250, "", 0,"transparent");
+
+
     }
 }
