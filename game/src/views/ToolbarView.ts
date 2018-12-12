39b04d09304054030f451ed69c11b4fb702a5616
DjordyKoert <djordy.koert@live.nl>
2018-12-12 22:12:11 +0100

    created get and set function for tool

--- game/src/views/ToolbarView.ts
+++ game/src/views/ToolbarView.ts
@@ -3,35 +3,38 @@ class ToolbarView {
     protected CanvasHelper: CanvasHelper
     private _mouseHelper: MouseHelper
     private clicked: boolean
-    private curTool: string
+    private static curTool: string
     private rendered: boolean
     public constructor(canvas: CanvasHelper) {
         this.CanvasHelper = canvas
         this.clicked = this.rendered = false
         this._mouseHelper = new MouseHelper()
-
+        ToolbarView.setTool(undefined)
     }
     public renderToolbar() {
-        if (!this.rendered) {
-            this.CanvasHelper.createRect(this.CanvasHelper.getWidth() * 0.2, this.CanvasHelper.getHeight() * 0.8, this.CanvasHelper.getWidth() * 0.6, this.CanvasHelper.getHeight() * 0.2)
-            this.CanvasHelper.createRect(this.CanvasHelper.getWidth() * 0.21, this.CanvasHelper.getHeight() * 0.81, this.CanvasHelper.getWidth() * 0.1, this.CanvasHelper.getHeight() * 0.18, "red")
-            this.rendered = true
-        }
-        this.setTool()
-        console.log(this.curTool)
+        this.CanvasHelper.createRect(this.CanvasHelper.getWidth() * 0.2, this.CanvasHelper.getHeight() * 0.8, this.CanvasHelper.getWidth() * 0.6, this.CanvasHelper.getHeight() * 0.2)
+        this.CanvasHelper.createRect(this.CanvasHelper.getWidth() * 0.21, this.CanvasHelper.getHeight() * 0.81, this.CanvasHelper.getWidth() * 0.1, this.CanvasHelper.getHeight() * 0.18, "red")
+        this.rendered = true
+
+        this.toolBarClick()
     }
 
-    public setTool(): void {
+    public toolBarClick(): void {
         if (this._mouseHelper.getClick().click && !this.clicked) {
             if (this._mouseHelper.getClick().x >= this.CanvasHelper.getWidth() * 0.21 && this._mouseHelper.getClick().x <= (this.CanvasHelper.getWidth() * 0.21 + this.CanvasHelper.getWidth() * 0.1)) {
                 if (this._mouseHelper.getClick().y >= this.CanvasHelper.getHeight() * 0.81 && this._mouseHelper.getClick().y <= (this.CanvasHelper.getHeight() * 0.81 + this.CanvasHelper.getHeight() * 0.18)) {
-                    if (this.curTool == "axe") {this.clicked = true;this.curTool = undefined; return }
+                    if (ToolbarView.getTool() == "axe") { this.clicked = true; ToolbarView.setTool(undefined); return }
                     this.clicked = true
-                    this.curTool = "axe"
+                    ToolbarView.setTool("axe")
                 }
             }
         }
         if (!this._mouseHelper.getClick().click) this.clicked = false
     }
-
+    public static setTool(tool: string) {
+        this.curTool = tool
+    }
+    public static getTool(): string {
+        return this.curTool
+    }
 }
