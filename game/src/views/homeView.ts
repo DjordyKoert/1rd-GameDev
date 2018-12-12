0938ca63e994f7f3ffa195efe3d0d4079f7b5db6
ArondeRijke <aronderijke@gmail.com>
2018-12-12 10:36:45 +0100

    push voor de bois

--- game/src/views/homeView.ts
+++ game/src/views/homeView.ts
@@ -1,21 +1,50 @@
 class HomeView {
     protected _screen: string = "homeScreen";
-    protected CanvasHelper: CanvasHelper
+    protected CanvasHelper: CanvasHelper;
+    protected imageCenter: number;
+    protected image: HTMLImageElement;
+    
 
     public constructor(canvas: CanvasHelper){
-        this.CanvasHelper = canvas
+        this.CanvasHelper = canvas;
+    }
+
+    public getImageWidth(){
+
     }
 
     public renderScreen(){
-        this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet.png", 250, 300, 300, 300)
-        this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet.png", 750, 400, 300, 300)
-        this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet.png", 1250, 200, 300, 300)
+        
+        const planetList: Array<string> = [
+            "./assets/images/temporary_textures/homeScreen_planet2.png",
+            "./assets/images/temporary_textures/homeScreen_planet2.png",
+            "./assets/images/temporary_textures/homeScreen_planet2.png",
+        ]
+        
+        const planetXCoords: Array<number> =[
+            250,
+            750,
+            1250
+        ]
+        const planetYCoords: Array<number> =[
+            300,
+            400,
+            200
+        ]
+        const maxPlanets: number = 3;
+
+        for (let i = 0; i < maxPlanets; i++) {
+            this.CanvasHelper.writeImageToCanvas(planetList[i], planetXCoords[i], planetYCoords[i], 300, 300)
+        }
+        // this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet2.png", 250, 300, 300, 300)
+        // this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet2.png", 750, 400, 300, 300)
+        // this.CanvasHelper.writeImageToCanvas("./assets/images/temporary_textures/homeScreen_planet2.png", 1250, 200, 300, 300)
         
         this.CanvasHelper.writeButtonToCanvas(0,0,150,100, "BACK", 20);
-        this.CanvasHelper.writeButtonToCanvas(275,325,250,250, "", 0,"transparent");
-        this.CanvasHelper.writeButtonToCanvas(775,425,250,250, "", 0, "transparent");
-        this.CanvasHelper.writeButtonToCanvas(1275,225,250,250, "", 0,"transparent");
+        // this.CanvasHelper.writeButtonToCanvas(275,325,250,250, "", 0,"transparent");
+        // this.CanvasHelper.writeButtonToCanvas(775,425,250,250, "", 0, "transparent");
+        // this.CanvasHelper.writeButtonToCanvas(1275,225,250,250, "", 0,"transparent");
 
-
+        this.getImageWidth()
     }
 }
