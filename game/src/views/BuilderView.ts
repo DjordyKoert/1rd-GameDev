adfdd4c83671a9be8b5a6b2a70949c9b64105a17
ever0035 <ever0035@hz.nl>
2018-12-12 10:39:31 +0100

    hele mooi push voor papa djordy

--- game/src/views/BuilderView.ts
+++ game/src/views/BuilderView.ts
@@ -1,8 +1,22 @@
 class BuilderView{
 
-    protected CanvasHelper: CanvasHelper
+    protected _canvasHelper: CanvasHelper
+    
+    private _viewWidth: number = 300
+    private _yPosLine1: number = 70
 
     public constructor(canvas: CanvasHelper){
-        this.CanvasHelper = canvas
+        this._canvasHelper = canvas
+    }
+
+    public renderScreen(){
+
+        this._canvasHelper.createRect(this._canvasHelper.getWidth()- this._viewWidth ,0 ,this._viewWidth, this._canvasHelper.getHeight(),'#736acc')
+        this._canvasHelper.writeTextToCanvas('GEBOUWEN',48,(this._canvasHelper.getWidth() - this._viewWidth / 2), 40,)
+        this._canvasHelper.moveTo(this._canvasHelper.getWidth() - this._viewWidth, this._yPosLine1)
+        this._canvasHelper.lineTo(this._canvasHelper.getWidth(), this._yPosLine1)
+        this._canvasHelper.writeTextToCanvas('HUIS', 36,(this._canvasHelper.getWidth() - this._viewWidth + 10), 120, 'black', 'left')
+        this._canvasHelper.writeTextToCanvas(`HEHE`,24,(this._canvasHelper.getWidth() - this._viewWidth + 10),155,'black', 'left')
+        this._canvasHelper.writeImageToCanvas('./assets/images/houses/house.png', (this._canvasHelper.getWidth() - this._viewWidth + 150), 90, 128,102)
     }
 }
