e81f953b746a7701856a2fa3760bbe8184940684
DjordyKoert <djordy.koert@live.nl>
2018-12-12 22:17:12 +0100

    UI stats on top, loadingbar for klimaat, remove houses with axe tool

--- game/src/views/GameView.ts
+++ game/src/views/GameView.ts
@@ -34,11 +34,13 @@ class GameView {
             "./assets/images/houses/house.png",
         ]
         this.tileInfo = [{}]
-       // this.renderGrid()
+        // this.renderGrid()
     }
     public renderScreen() {
-        if (!this.gridsRendered) {this.renderGrid(); this.gridsRendered = true}
+        if (!this.gridsRendered) { this.renderGrid(); this.gridsRendered = true }
         this._ToolbarView.renderToolbar()
+        this._UIView.renderScreen()
+        this.CanvasHelper.loadingBar(400, 300, 100, 20, App._klimaat, 100)
     }
     public renderGrid() {
         for (let line = 0; line < this.lines; line++) {
@@ -58,6 +60,26 @@ class GameView {
             this.xCoord += this.sqSize
             this.yCoord += this.sqSize
         }
-        this._UIView.renderScreen()
+        //Clean this.tileInfo so it only contains info about things INSIDE the screen
+        this.tileInfo = this.tileInfo.filter(x => x.xStart <= this.CanvasHelper.getWidth() && x.yStart <= this.CanvasHelper.getHeight())
+        let allHouses = this.tileInfo.filter(x => x.imageSrc == this.tileImages[1])
+        allHouses.forEach(() => {
+            App._klimaat += 1
+        });
+        window.addEventListener("mousedown", e => {
+            if (ToolbarView.getTool() == "axe") {
+                let filter = this.tileInfo.find(x => e.x >= x.xStart && e.x <= x.xEnd && e.y >= x.yStart && e.y <= x.yEnd)
+                if (!filter) return;
+                if (filter.imageSrc == "./assets/images/houses/house.png") {
+                    this.CanvasHelper.writeImageToCanvas(this.tileImages[0], filter.xStart, filter.yStart, filter.xEnd - filter.xStart, filter.yEnd - filter.yStart)
+                    let n = this.tileInfo.findIndex(x => e.x >= x.xStart && e.x <= x.xEnd && e.y >= x.yStart && e.y <= x.yEnd)
+                    this.tileInfo.splice(n, 1)
+                    let vr = { xStart: filter.xStart, xEnd: filter.xEnd - filter.xStart + this.sqSize, yStart: filter.yStart, yEnd: filter.yEnd - filter.yStart, imageSrc: this.tileImages[0] }
+                    this.tileInfo.push(vr)
+                    App._gold += 6
+                    App._klimaat -= 1
+                }
+            }
+        })
     }
 }
