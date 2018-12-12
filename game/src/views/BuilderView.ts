fa68adcb8b63de5db1cbc8f7c70c8332650498e9
ever0035 <ever0035@hz.nl>
2018-12-12 11:30:49 +0100

    Papa Djordy notice me

--- game/src/views/BuilderView.ts
+++ game/src/views/BuilderView.ts
@@ -20,9 +20,8 @@ class BuilderView{
         this._canvasHelper.lineTo(this._canvasHelper.getWidth(), this._yPosLine1)
         this._canvasHelper.writeTextToCanvas('HUIS', 36,(this._canvasHelper.getWidth() - this._viewWidth + 10), 100, 'black', 'left')
         this._canvasHelper.writeTextToCanvas(`Muns: 50`,24,(this._canvasHelper.getWidth() - this._viewWidth + 10),135,'black', 'left')
-        // this._canvasHelper.writeImageToCanvas('./assets/images/houses/house.png', (this._canvasHelper.getWidth() - this._viewWidth + 150), 90, 128,102)
+        this._canvasHelper.writeImageToCanvas('./assets/images/houses/house.png', (this._canvasHelper.getWidth() - this._viewWidth + 150), 90, 128,102)
         this._canvasHelper.moveTo(this._canvasHelper.getWidth() - this._viewWidth, this._yPosLine2)
         this._canvasHelper.lineTo(this._canvasHelper.getWidth(), this._yPosLine2)
-        
     }
 }
