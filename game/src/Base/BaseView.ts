6cdfac3911d236e7b8cc5a97aea6e3c157e6f502
DjordyKoert <djordy.koert@live.nl>
2018-12-12 12:39:52 +0100

    UIView and Builderview are inside gamview now

--- game/src/Base/BaseView.ts
+++ game/src/Base/BaseView.ts
@@ -8,12 +8,10 @@ class BaseView {
         this._canvasHelper = new CanvasHelper(canvas)
         
         this._homeView = new HomeView(this._canvasHelper)
-        this._BuilderView = new BuilderView(this._canvasHelper)
         this._GameView = new GameView(this._canvasHelper)
-        this._UIView = new UIView(this._canvasHelper)
         //this._homeView.homeScreen()
     }
     public render() {
-        this._homeView.renderScreen()
+        this._GameView.renderScreen()
     }
 }
