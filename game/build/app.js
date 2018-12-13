28a61ab14d3952dd9d41f19fca86c33a361d6910
DjordyKoert <djordy.koert@live.nl>
2018-12-13 11:27:03 +0100

    app

--- game/build/app.js
+++ game/build/app.js
@@ -177,12 +177,12 @@ class GameView {
         this._screen = "gameScreen";
         this.CanvasHelper = canvas;
         this._mouseHelper = new MouseHelper();
-        this._BuilderView = new BuilderView(canvas);
         this._UIView = new UIView(canvas);
         this._ToolbarView = new ToolbarView(canvas);
         this.gridsRendered = false;
         this.xCoord = this.yCoord = 0;
         this.lines = 10;
+        this.BuilderViewOn = false;
         if (this.CanvasHelper.getWidth() > this.CanvasHelper.getHeight()) {
             this.sqSize = this.CanvasHelper.getWidth() / this.lines;
         }
@@ -196,57 +196,78 @@ class GameView {
         this.tileInfo = [{}];
     }
     renderScreen() {
-        if (!this.gridsRendered) {
-            this.renderGrid();
-            this.gridsRendered = true;
-        }
+        console.log("da");
+        this.renderGrid();
         this._ToolbarView.renderToolbar();
         this._UIView.renderScreen();
-        this.CanvasHelper.writeTextToCanvas("Djordy was hier ;), ik heb gekut met een muziekje", 90, this.CanvasHelper.getCenter().X, this.CanvasHelper.getCenter().Y, "red", "center");
+        if (this.BuilderViewOn)
+            this._BuilderView.renderScreen();
         this.CanvasHelper.loadingBar(400, 300, 100, 20, App._klimaat, 100);
     }
     renderGrid() {
-        let music = new Audio("./assets/sounds/yeet2.mp3");
-        music.play();
-        setTimeout(() => {
-            let music2 = new Audio("./assets/sounds/yeet.mp3");
-            music2.play();
-        }, 15000);
-        for (let line = 0; line < this.lines; line++) {
-            this.CanvasHelper.moveTo(0, this.yCoord);
-            this.CanvasHelper.lineTo(this.CanvasHelper.getWidth(), this.yCoord);
-            this.CanvasHelper.moveTo(this.xCoord, 0);
-            this.CanvasHelper.lineTo(this.xCoord, this.CanvasHelper.getHeight());
-            for (let i = 0; i < this.lines; i++) {
-                let imageSrc = this.tileImages[MathHelper.randomNumber(0, this.tileImages.length - 1)];
-                this.CanvasHelper.writeImageToCanvas(imageSrc, this.xCoord, this.sqSize * i, this.sqSize, this.sqSize);
-                let vr = { xStart: this.xCoord, xEnd: this.xCoord + this.sqSize, yStart: this.sqSize * i, yEnd: (this.sqSize * i) + this.sqSize, imageSrc: imageSrc };
-                this.tileInfo.push(vr);
+        this.CanvasHelper._context.beginPath();
+        if (this.gridsRendered) {
+            this.xCoord = 0;
+            this.yCoord = 0;
+            for (let line = 0; line < this.lines; line++) {
+                this.CanvasHelper.moveTo(0, this.yCoord);
+                this.CanvasHelper.lineTo(this.CanvasHelper.getWidth(), this.yCoord);
+                this.CanvasHelper.moveTo(this.xCoord, 0);
+                this.CanvasHelper.lineTo(this.xCoord, this.CanvasHelper.getHeight());
+                this.tileInfo.forEach(tile => {
+                    this.CanvasHelper.writeImageToCanvas(tile.imageSrc, tile.xStart, tile.yStart, tile.xEnd - tile.xStart, tile.yEnd - tile.yStart);
+                });
             }
-            this.xCoord += this.sqSize;
-            this.yCoord += this.sqSize;
         }
-        this.tileInfo = this.tileInfo.filter(x => x.xStart <= this.CanvasHelper.getWidth() && x.yStart <= this.CanvasHelper.getHeight());
-        let allHouses = this.tileInfo.filter(x => x.imageSrc == this.tileImages[1]);
-        allHouses.forEach(() => {
-            App._klimaat += 1;
-        });
-        window.addEventListener("mousedown", e => {
-            if (ToolbarView.getTool() == "axe") {
-                let filter = this.tileInfo.find(x => e.x >= x.xStart && e.x <= x.xEnd && e.y >= x.yStart && e.y <= x.yEnd);
-                if (!filter)
-                    return;
-                if (filter.imageSrc == "./assets/images/houses/house.png") {
-                    this.CanvasHelper.writeImageToCanvas(this.tileImages[0], filter.xStart, filter.yStart, filter.xEnd - filter.xStart, filter.yEnd - filter.yStart);
-                    let n = this.tileInfo.findIndex(x => e.x >= x.xStart && e.x <= x.xEnd && e.y >= x.yStart && e.y <= x.yEnd);
-                    this.tileInfo.splice(n, 1);
-                    let vr = { xStart: filter.xStart, xEnd: filter.xEnd - filter.xStart + this.sqSize, yStart: filter.yStart, yEnd: filter.yEnd - filter.yStart, imageSrc: this.tileImages[0] };
+        else {
+            for (let line = 0; line < this.lines; line++) {
+                this.CanvasHelper.moveTo(0, this.yCoord);
+                this.CanvasHelper.lineTo(this.CanvasHelper.getWidth(), this.yCoord);
+                this.CanvasHelper.moveTo(this.xCoord, 0);
+                this.CanvasHelper.lineTo(this.xCoord, this.CanvasHelper.getHeight());
+                for (let i = 0; i < this.lines; i++) {
+                    let imageSrc = this.tileImages[MathHelper.randomNumber(0, this.tileImages.length - 1)];
+                    console.log("da");
+                    this.CanvasHelper.writeImageToCanvas(imageSrc, this.xCoord, this.sqSize * i, this.sqSize, this.sqSize);
+                    let vr = { xStart: this.xCoord, xEnd: this.xCoord + this.sqSize, yStart: this.sqSize * i, yEnd: (this.sqSize * i) + this.sqSize, imageSrc: imageSrc };
                     this.tileInfo.push(vr);
-                    App._gold += 6;
-                    App._klimaat -= 1;
                 }
+                this.xCoord += this.sqSize;
+                this.yCoord += this.sqSize;
             }
-        });
+            this.tileInfo = this.tileInfo.filter(x => x.xStart <= this.CanvasHelper.getWidth() && x.yStart <= this.CanvasHelper.getHeight());
+            let allHouses = this.tileInfo.filter(x => x.imageSrc == this.tileImages[1]);
+            allHouses.forEach(() => {
+                App._klimaat += 1;
+            });
+            window.addEventListener("mousedown", e => {
+                if (ToolbarView.getTool() == "axe") {
+                    let filter = this.tileInfo.find(x => e.x >= x.xStart && e.x <= x.xEnd && e.y >= x.yStart && e.y <= x.yEnd);
+                    if (!filter)
+                        return;
+                    if (filter.imageSrc == "./assets/images/houses/house.png") {
+                        this.CanvasHelper.writeImageToCanvas(this.tileInfo[0], filter.xStart, filter.yStart, filter.xEnd - filter.xStart, filter.yEnd - filter.yStart);
+                        let n = this.tileInfo.findIndex(x => e.x >= x.xStart && e.x <= x.xEnd && e.y >= x.yStart && e.y <= x.yEnd);
+                        this.tileInfo.splice(n, 1);
+                        let vr = { xStart: filter.xStart, xEnd: filter.xEnd - filter.xStart + this.sqSize, yStart: filter.yStart, yEnd: filter.yEnd - filter.yStart, imageSrc: this.tileImages[0] };
+                        this.tileInfo.push(vr);
+                        App._gold += 6;
+                        App._klimaat -= 1;
+                    }
+                }
+            });
+            window.addEventListener("keypress", press => {
+                if (press.key == " ") {
+                    if (this.BuilderViewOn) {
+                        this.BuilderViewOn = false;
+                        this.CanvasHelper.clear();
+                    }
+                    else
+                        this.BuilderViewOn = true;
+                }
+            });
+            this.gridsRendered = true;
+        }
     }
 }
 class ToolbarView {
