259bb12ac5e0d71854226e792de9ecde28d9ab3f
DjordyKoert <djordy.koert@live.nl>
2018-12-17 19:46:11 +0100

    moved screen to app.ts

--- game/src/Base/BaseView.ts
+++ game/src/Base/BaseView.ts
@@ -1,11 +1,9 @@
 class BaseView {
-    public curScreen: string
     protected _canvasHelper: CanvasHelper
     protected _mouseHelper: MouseHelper
-    public constructor(canvas: HTMLCanvasElement, screen:string){
+    public constructor(canvas: HTMLCanvasElement){
         this._canvasHelper = new CanvasHelper(canvas)
         this._mouseHelper = new MouseHelper()
-        this.curScreen = screen
         //this._homeView.homeScreen()
     }
 
