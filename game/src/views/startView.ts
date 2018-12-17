77417640de3769b41cf1d7d47cac378d7cf71627
ArondeRijke <aronderijke@gmail.com>
2018-12-17 13:00:28 +0100

    fixed startscreen and added new house background texture

--- game/src/views/startView.ts
+++ game/src/views/startView.ts
@@ -1,40 +1,51 @@
 class StartView extends BaseView {
-    private CanvasHelper: CanvasHelper
+    protected _canvasHelper: CanvasHelper
     private _rendered: boolean = false;
     private _clicked: boolean = false;
+    protected _homeView: HomeView
+
 
     public constructor(canvas: HTMLCanvasElement) {
         super(canvas, "start")
+        this._homeView = new HomeView(canvas)
     }
 
     public renderScreen() {
+        this.drawTitle();
+        this.drawStartButton();
+        this.buttonClick();
+        console.log("start")
+     }
 
 
-        this.CanvasHelper.clear()
+    public drawTitle() {
+        this._canvasHelper.writeTextToCanvas("SAMPLE TEXT", 50, (this._canvasHelper.getWidth() / 2), 100, "white")
+    }
 
-        this.CanvasHelper.writeTextToCanvas("SAMPLE TEXT", 50, (this.CanvasHelper.getWidth() / 2), 100, "white")
+    public drawStartButton() {
+        this._canvasHelper.createRect((this._canvasHelper.getWidth() / 2) - 150, (this._canvasHelper.getHeight() / 2) - 100, 300, 200)
+        this._canvasHelper.writeTextToCanvas("START SPEL", 30, this._canvasHelper.getWidth() / 2, this._canvasHelper.getHeight() / 2, "black")
+    }
 
-        this.CanvasHelper.createRect((this.CanvasHelper.getWidth() / 2) - 150, (this.CanvasHelper.getHeight() / 2) - 100, 300, 200)
-        this.CanvasHelper.writeTextToCanvas("START SPEL", 30, this.CanvasHelper.getWidth() / 2, this.CanvasHelper.getHeight() / 2, "black")
-
+    public buttonClick() {
         if (this._mouseHelper.getClick().click && !this._clicked) {
+            // received a mouse down event
+            this._clicked = true;
+        }
 
-            if (this._mouseHelper.getClick().x > (this.CanvasHelper.getWidth() / 2) - 150 && this._mouseHelper.getClick().x < (this.CanvasHelper.getWidth() / 2) + 150) {
-                if (this._mouseHelper.getClick().y > (this.CanvasHelper.getHeight() / 2) - 100 && this._mouseHelper.getClick().y < (this.CanvasHelper.getHeight() / 2) + 100) {
-                    this._clicked = true
-                    console.log("b")
+        if (!this._mouseHelper.getClick().click && this._clicked) {
+            // receive a mouse up event after mouse down
+            this._clicked = false;
+            
+                console.log(this._clicked);
+                if (this._mouseHelper.getClick().x > (this._canvasHelper.getWidth() / 2) - 150 && this._mouseHelper.getClick().x < (this._canvasHelper.getWidth() / 2) + 150) {
+                    if (this._mouseHelper.getClick().y > (this._canvasHelper.getHeight() / 2) - 100 && this._mouseHelper.getClick().y < (this._canvasHelper.getHeight() / 2) + 100) {
+                        this._canvasHelper.clear()
+                        this._homeView.renderScreen();
                 }
             }
         }
-        if (!this._mouseHelper.getClick().click && this._clicked) {
-            console.log("a")
-            this._clicked = false
-            this.CanvasHelper.clear();
-            this.curScreen = "home"
-            event.preventDefault()
-
-        }
-        this.CanvasHelper.createRect((this.CanvasHelper.getWidth() / 2) - 150, (this.CanvasHelper.getHeight() / 2) + 500, 300, 200)
+    
     }
 
 
