0b5e96d99d3d313bc5a3c8843de6e86f3622f45d
DjordyKoert <djordy.koert@live.nl>
2018-12-21 09:04:14 +0100

    added canvasOverlay

--- game/src/Base/BaseView.ts
+++ game/src/Base/BaseView.ts
@@ -1,8 +1,10 @@
 class BaseView {
     protected _canvasHelper: CanvasHelper
+    protected _canvasHelperOverlay: CanvasHelper
     protected _mouseHelper: MouseHelper
     public constructor(canvas: HTMLCanvasElement){
         this._canvasHelper = new CanvasHelper(canvas)
+        this._canvasHelperOverlay = new CanvasHelper(<HTMLCanvasElement>document.getElementById("canvasOverlay"))
         this._mouseHelper = new MouseHelper()
         //this._homeView.homeScreen()
     }
