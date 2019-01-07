2f7831ff447151b7288ffb593309a7cccd69d8ea
ever0035 <ever0035@hz.nl>
2019-01-07 11:11:12 +0100

    startview  button aangepast

--- game/src/views/startView.ts
+++ game/src/views/startView.ts
@@ -3,27 +3,31 @@ class StartView extends BaseView {
     private _rendered: boolean = false;
     private _clicked: boolean = false;
     protected _homeView: HomeView
+    private _buttonDimension: Array<number>
 
+    private _gameName: string = 'PLACEHOLDER'
 
     public constructor(canvas: HTMLCanvasElement) {
         super(canvas)
         this._homeView = new HomeView(canvas)
+        this._buttonDimension = [225, 125]
     }
 
     public renderScreen() {
         this.drawTitle();
         this.drawStartButton();
         this.buttonClick();
-        console.log("start")
-     }
+        // console.log("start")
+    }
 
 
     public drawTitle() {
-        this._canvasHelper.writeTextToCanvas("SAMPLE TEXT", 50, (this._canvasHelper.getWidth() / 2), 100, "white")
+        this._canvasHelper.writeTextToCanvas(this._gameName, 50, (this._canvasHelper.getWidth() / 2), 100, "white")
     }
 
     public drawStartButton() {
-        this._canvasHelper.createRect((this._canvasHelper.getWidth() / 2) - 150, (this._canvasHelper.getHeight() / 2) - 100, 300, 200)
+        this._canvasHelper.createRect((this._canvasHelper.getWidth() / 2) - (this._buttonDimension[0] / 2), (this._canvasHelper.getHeight() / 2) - (this._buttonDimension[1] / 2), this._buttonDimension[0] + 2, this._buttonDimension[1] + 2, "black")
+        this._canvasHelper.createRect((this._canvasHelper.getWidth() / 2) - (this._buttonDimension[0] / 2), (this._canvasHelper.getHeight() / 2) - (this._buttonDimension[1] / 2), this._buttonDimension[0], this._buttonDimension[1])
         this._canvasHelper.writeTextToCanvas("START SPEL", 30, this._canvasHelper.getWidth() / 2, this._canvasHelper.getHeight() / 2, "black")
     }
 
@@ -36,19 +40,29 @@ class StartView extends BaseView {
         if (!this._mouseHelper.getClick().click && this._clicked) {
             // receive a mouse up event after mouse down
             this._clicked = false;
-            
-                console.log(this._clicked);
-                if (this._mouseHelper.getClick().x > (this._canvasHelper.getWidth() / 2) - 150 && this._mouseHelper.getClick().x < (this._canvasHelper.getWidth() / 2) + 150) {
-                    if (this._mouseHelper.getClick().y > (this._canvasHelper.getHeight() / 2) - 100 && this._mouseHelper.getClick().y < (this._canvasHelper.getHeight() / 2) + 100) {
-                        this._canvasHelper.clear()
-                        App._screen = "home"
+        }
+
+        // console.log(this._clicked);
+        if (this._clicked) {
+            if (this._mouseHelper.getClick().x > (this._canvasHelper.getWidth() / 2) - (this._buttonDimension[0] / 2) && this._mouseHelper.getClick().x < (this._canvasHelper.getWidth() / 2) + (this._buttonDimension[0] / 2)) {
+                if (this._mouseHelper.getClick().y > (this._canvasHelper.getHeight() / 2) - (this._buttonDimension[1] / 2) && this._mouseHelper.getClick().y < (this._canvasHelper.getHeight() / 2) + (this._buttonDimension[1] / 2)) {
+                    // console.log('pressed')
                 }
             }
         }
-    
+        if (!this._clicked) {
+            if (this._mouseHelper.getClick().x > (this._canvasHelper.getWidth() / 2) - (this._buttonDimension[0] / 2) && this._mouseHelper.getClick().x < (this._canvasHelper.getWidth() / 2) + (this._buttonDimension[0] / 2)) {
+                if (this._mouseHelper.getClick().y > (this._canvasHelper.getHeight() / 2) - (this._buttonDimension[1] / 2) && this._mouseHelper.getClick().y < (this._canvasHelper.getHeight() / 2) + (this._buttonDimension[1] / 2)) {
+                    // console.log('released')
+                    this._canvasHelper.clear
+                    App._screen = "home"
+                }
+            }
+        }
+
     }
 
-
 }
+
 
 
