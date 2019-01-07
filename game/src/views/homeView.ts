39b309d9dbaaddc9851bcdf4cea7585ff290c613
ever0035 <ever0035@hz.nl>
2019-01-07 14:42:25 +0100

    homeview verschoond en back button functie verkend gemaakt

--- game/src/views/homeView.ts
+++ game/src/views/homeView.ts
@@ -1,82 +1,69 @@
 class HomeView extends BaseView {
     protected _screen: string = "homeScreen";
-    protected imageCenter: number;
-    private _rendered: boolean = false;
-    private MouseHelper: MouseHelper
-    private planetList: Array<string>
-    private planetXCoords: Array<number>
-    private planetYCoords: Array<number>
+    protected _imageCenter: number;
+    private _rendered: boolean
+    protected _mouseHelper: MouseHelper
+    private _planetList: string
+    private _planetXcoord: number
+    private _planetYcoord: number
     private _gameView: GameView
-    private clicked: boolean
+    private _clicked: boolean
+
+
 
     public constructor(canvas: HTMLCanvasElement) {
         super(canvas)
-        this.MouseHelper = new MouseHelper()
+        this._mouseHelper = new MouseHelper()
         this._gameView = new GameView(canvas)
-        this.clicked = false
-        this.planetList = [
-            "./assets/images/temporary_textures/homeScreen_planet2.png",
+        this._clicked = false
+        this._planetList = "./assets/images/temporary_textures/homeScreen_planet2.png",
+            this._planetXcoord = (this._canvasHelper.getWidth() / 2) - 250
+        this._planetYcoord = (this._canvasHelper.getHeight() / 2) - 250
 
-        ]
 
-        this.planetXCoords = [
-            this._canvasHelper.getWidth() / 2 - 250,
-        ]
-
-        this.planetYCoords = [
-            200
-        ]
     }
 
     public renderScreen(): void {
-        this.drawPlanets();
-        this.drawBackButton();
+        if (!this._rendered) {
+            this.drawPlanets();
+            this.drawBackButton();
+        }
+        this._rendered = true
         this.screenClick();
         // console.log("home rendered")
     }
 
     public drawPlanets() {
-        const maxPlanets: number = 3;
-
-        for (let i = 0; i < maxPlanets; i++) {
-            this._canvasHelper.writeImageToCanvas(this.planetList[i], this.planetXCoords[i], this.planetYCoords[i], 500, 500)
-
-
-            this._canvasHelper.writeTextToCanvas("new world", 50, this.planetXCoords[i] + 250, this.planetYCoords[i] + 540)
-        }
+        this._canvasHelper.writeImageToCanvas(this._planetList, this._planetXcoord, this._planetYcoord, 500, 500)
+        this._canvasHelper.writeTextToCanvas("Nieuwe Wereld", 50, this._planetXcoord + 250, this._planetYcoord + 540)
     }
 
     public drawBackButton() {
-
+        this._canvasHelper.createRect(0, 0, 152, 102, 'green')
         this._canvasHelper.createRect(0, 0, 150, 100)
-        this._canvasHelper.writeTextToCanvas("BACK", 30, 75, 50, "black")
-        if (this.MouseHelper.getClick().x > 0 && this.MouseHelper.getClick().x < 150) {
-            if (this.MouseHelper.getClick().y > 0 && this.MouseHelper.getClick().y < 100) {
-                //this.curScreen = "start"
-                this._canvasHelper.clear()
-            }
-        }
+        this._canvasHelper.writeTextToCanvas("TERUG", 30, 75, 50, "black")
     }
 
     public screenClick() {
-        if (this.MouseHelper.getClick().click && !this.clicked) {
+        if (this._mouseHelper.getClick().click && !this._clicked) {
             // received a mouse down event
-            this.clicked = true;
+            this._clicked = true;
         }
 
-        if (!this.MouseHelper.getClick().click && this.clicked) {
+        if (!this._mouseHelper.getClick().click && this._clicked) {
             // receive a mouse up event after mouse down
-            this.clicked = false;
-            for (let i = 0; i < this.planetList.length; i++) {
-                // console.log(this.clicked);
-                if (this.MouseHelper.getClick().x > this.planetXCoords[i] && this.MouseHelper.getClick().x < this.planetXCoords[i] + 500) {
-                    if (this.MouseHelper.getClick().y > this.planetYCoords[i] && this.MouseHelper.getClick().y < this.planetYCoords[i] + 500) {
-                        let person = prompt("Please enter your name", "");
+            this._clicked = false;
+
+            if (!this._clicked) {
+                if (this._mouseHelper.getClick().x > this._planetXcoord && this._mouseHelper.getClick().x < this._planetXcoord + 500) {
+                    if (this._mouseHelper.getClick().y > this._planetYcoord && this._mouseHelper.getClick().y < this._planetYcoord + 500) {
+                        console.log('planet clicked')
+                        let person = prompt("Voer je naam in", "");
                         if (person == null || person == "") {
-                            window.alert("voer eerst een naam in")
+                            window.alert("Voer een naam in")
                         }
                         else {
-                            if(person.length > 10 ){
+                            if (person.length > 10) {
                                 window.alert("je naam mag maximaal 10 letters lang zijn");
                                 return
                             }
@@ -85,6 +72,24 @@ class HomeView extends BaseView {
                             App._name = person
                         }
                     }
+                }
+            }
+        }
+
+        if (this._clicked) {
+            if (this._mouseHelper.getClick().x > 0 && this._mouseHelper.getClick().x < 150) {
+                if (this._mouseHelper.getClick().y > 0 && this._mouseHelper.getClick().y < 100) {
+                    this._canvasHelper.createRect(0, 0, 152, 102)
+                    this._canvasHelper.writeTextToCanvas("TERUG", 30, 77, 52, "black")
+                }
+            }
+        }
+        if (!this._clicked) {
+            if (this._mouseHelper.getClick().x > 0 && this._mouseHelper.getClick().x < 150) {
+                if (this._mouseHelper.getClick().y > 0 && this._mouseHelper.getClick().y < 100) {
+                    this._canvasHelper.clear()
+                    this._rendered = false
+                    App._screen = 'start'
                 }
             }
         }
